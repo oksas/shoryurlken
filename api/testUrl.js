1386fc0c0bfe4be4117ad027c4af92e7e320599e
John Oksasoglu <john.oksasoglu@gmail.com>
2016-03-01 20:18:20 +0000

    Add error message for bad URLS

--- api/testUrl.js
+++ api/testUrl.js
@@ -6,7 +6,7 @@ module.exports = function testUrl(url, callback) {
     
     request(url, function(err, res) {
         if (err) {
-            return callback(err);
+            return callback("Not a valid URL. Did you spell it right?");
         }
         
         callback(null);

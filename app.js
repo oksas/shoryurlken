17e4ac76d41f53b608755df7f290c1df68220f97
John Oksasoglu <john.oksasoglu@gmail.com>
2016-03-01 02:08:24 +0000

    Add basic index page

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 "use strict";
 
 const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
 const app = express();
 
 const cleanUrl = require("./api/cleanUrl");
@@ -9,9 +11,36 @@ const shortenUrl = require("./api/shortenUrl");
 
 const port = process.env.PORT || 2020;
 
-app.get("/shorten/", function(req, res) {
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res) {
+   res.render("index", {title: "sup dawg"}); 
+});
+
+app.get("/shorten", function(req, res) {
     
     const url = cleanUrl(req.query.site);
+    
+    testUrl(url, function(err) {
+        if (err) {
+            return res.send(`ERROR: ${err}`);
+        }
+        
+        shortenUrl(url, function(entry) {
+            console.log("GOT THE ENTRY!");
+            console.log(entry);
+            
+            res.send(entry.word);
+        });
+        
+    });
+});
+
+app.post("/shorten", urlencodedParser, function(req, res) {
+    
+    const url = cleanUrl(req.body.url);
     
     testUrl(url, function(err) {
         if (err) {
